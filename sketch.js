cd1e3b6efdb0e61f0b8bdae30f74b47b5d869fce
SarbagyaKha <79778171+SarbagyaKha@users.noreply.github.com>
2021-05-16 21:30:57 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,12 @@
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var distance = 0;
+var database; 
+
+var car1, car2
+var track, car1.jpg, car2.jpg
+
+function preload()
+car1 = loadImage("../images/car1.png");
+car2 = loadImage("../images/car2.png");
